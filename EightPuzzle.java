09742381cddaf516efc51e81ccea17bbd265e7ac
Charlie Lin <clin10900@gmail.com>
2023-09-24 19:05:18 -0400

    Implement A*

--- EightPuzzle.java
+++ EightPuzzle.java
@@ -1,14 +1,27 @@
+
 import java.util.*;
 
-public class EightPuzzle {
+public class EightPuzzle implements Comparable<EightPuzzle> {
     // Using matrix to represent puzzle
     private int[][] grid;
 
-    // row of the space
+    // Row of the space
     private int r;
 
-    // column of the space
+    // Column of the space
     private int c;
+
+    // Reference to parent state 
+    private EightPuzzle parent;
+
+    // Previous move
+    private String prevMove;
+
+    // Value of state 
+    private int value;
+
+    // Max number of nodes to consider during search
+    private static int maxNodes = Integer.MAX_VALUE;
 
     /**
      * Constructs a new 8-Puzzle in the solved state
@@ -17,6 +30,18 @@ public class EightPuzzle {
         grid = new int[][] {{0,1,2}, {3,4,5}, {6,7,8}};
         r = 0;
         c = 0;
+        parent = null;
+        prevMove = null;
+        value = 0;
+    }
+
+    /**
+     * Set the max number of nodes to be considered during a search
+     * 
+     * @param n Number of nodes to consider
+     */
+    public static void setMaxNodes(int n) {
+        maxNodes = n;
     }
 
     /**
@@ -88,10 +113,10 @@ public class EightPuzzle {
     public boolean up() {
         if (r == 0) {
             return false;
-        } else {
-            grid[r][c] = grid[--r][c];
-            grid[r][c] = 0;
         }
+        grid[r][c] = grid[--r][c];
+        grid[r][c] = 0;
+        prevMove = "up";
         return true;
     }
 
@@ -103,10 +128,10 @@ public class EightPuzzle {
     public boolean down() {
         if (r == 2) {
             return false;
-        } else {
-            grid[r][c] = grid[++r][c];
-            grid[r][c] = 0;
         }
+        grid[r][c] = grid[++r][c];
+        grid[r][c] = 0;
+        prevMove = "down";
         return true;
     }
 
@@ -118,10 +143,10 @@ public class EightPuzzle {
     public boolean left() {
         if (c == 0) {
             return false;
-        } else {
-            grid[r][c] = grid[r][--c];
-            grid[r][c] = 0;
         }
+        grid[r][c] = grid[r][--c];
+        grid[r][c] = 0;
+        prevMove = "left";
         return true;
     }
 
@@ -133,10 +158,10 @@ public class EightPuzzle {
     public boolean right() {
         if (c == 2) {
             return false;
-        } else {
-            grid[r][c] = grid[r][++c];
-            grid[r][c] = 0;
         }
+        grid[r][c] = grid[r][++c];
+        grid[r][c] = 0;
+        prevMove = "right";
         return true;
     }
 
@@ -151,13 +176,13 @@ public class EightPuzzle {
         r = 0;
         c = 0;
         for (int i = 0; i < n; i++) {
-            List<Integer> moves = getValidMoves();
-            int move = moves.get((int)(Math.random()*moves.size()));
-            if (move == 1) {
+            List<String> moves = getValidMoves();
+            String move = moves.get((int)(Math.random()*moves.size()));
+            if (move.equals("up")) {
                 up();
-            } else if (move == 2) {
+            } else if (move.equals("down")) {
                 down();
-            }  else if (move == 3) {
+            }  else if (move.equals("left")) {
                 left();
             } else {
                 right();
@@ -166,25 +191,282 @@ public class EightPuzzle {
     }
 
     /**
-     * Helper method for randomize
+     * Returns a list of valid moves for given board state
      * 
      * @return List of valid moves
      */
-    private List<Integer> getValidMoves() {
-        List<Integer> moves = new LinkedList<>();
+    public List<String> getValidMoves() {
+        List<String> moves = new LinkedList<>();
         // can move up
         if (r != 0)
-            moves.add(1);
+            moves.add("up");
         // can move down
         if (r != 2)
-            moves.add(2);
+            moves.add("down");
         // can move left
         if (c != 0)
-            moves.add(3);
+            moves.add("left");
         // can move right
         if (c != 2)
-            moves.add(4);
+            moves.add("right");
         return moves;
+    }
+
+    /**
+     * Solves 8-puzzle using A* search and prints the solution
+     * Specify heuristic as "h1" (number of misplaced tiles) or "h2" (manhattan distance)
+     * 
+     * @param heuristic Either "h1" or "h2"
+     */
+    public void solveAStar(String heuristic) throws IllegalArgumentException {
+        if (heuristic.equals("h1")) {
+            System.out.println("Number of moves: " + solveH1());
+        } else if (heuristic.equals("h2")) {
+            System.out.println("Number of moves: " + solveH2());
+        } else {
+            throw new IllegalArgumentException("Invalid heuristic");
+        }
+    }
+
+    /**
+     * A* search using h1
+     * 
+     * @return Number of moves
+     * @throws OutOfMemoryError Max node limit exceeded
+     */
+    private int solveH1() throws OutOfMemoryError {
+        // min cost heap
+        PriorityQueue<EightPuzzle> pq = new PriorityQueue<>();
+
+        // keep track of visited states
+        Map<String, EightPuzzle> visited = new HashMap<>();
+
+        // count of generated nodes
+        int nodes = 0;
+
+        // add initial state to pq
+        pq.add(this);
+
+        while (!visited.containsKey("012345678") && !pq.isEmpty() && nodes <= maxNodes) {
+            EightPuzzle currState = pq.poll();
+            String gridString = gridToString(currState);
+            if (!visited.containsKey(gridString)) {
+                visited.put(gridString, currState);
+
+                List<String> validMoves = currState.getValidMoves();
+                for (String move : validMoves) {
+                    // generate and add child state if not already visited
+                    EightPuzzle child = currState.duplicate();
+                    if (move.equals("up")) {
+                        child.up();
+                    } else if (move.equals("down")) {
+                        child.down();
+                    } else if (move.equals("left")) {
+                        child.left();
+                    } else {
+                        child.right();
+                    }
+                    if (!visited.containsKey(gridToString(child))) {
+                        int depth = currState.value - h1(currState) + 1;
+                        child.parent = currState;
+                        child.value = depth + h1(child);
+                        pq.add(child);
+                        nodes++;
+                    }
+                }
+            }
+        }
+        
+        if (nodes > maxNodes) {
+            throw new OutOfMemoryError("Max node limit exceeded.");
+        }
+
+        // Extract path
+        EightPuzzle trav = null;
+        if (visited.containsKey("012345678")) {
+            trav = visited.get("012345678");
+            int moveCount = 0;
+            List<String> path = new LinkedList<>();
+            while (trav != null) {
+                path.add(trav.prevMove);
+                moveCount++;
+                trav = trav.parent;
+            }
+            path.remove(path.size()-1);
+            Collections.reverse(path);
+            System.out.println(path.toString());
+            return moveCount-1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Create deep copy of current puzzle state
+     * 
+     * @return Deep copy of puzzle
+     */
+    private EightPuzzle duplicate() {
+        EightPuzzle copy = new EightPuzzle();
+        int[][] copyGrid = new int[3][3];
+        int i = 0;
+        for (int[] row : grid) {
+            int j = 0;
+            for (int tile : row) {
+                copyGrid[i][j++] = tile;
+            }
+            i++;
+        }
+        copy.grid = copyGrid;
+        copy.c = c;
+        copy.r = r;
+        return copy;
+    }
+
+    /**
+     * Heuristic function based on number of misplaced tiles
+     * 
+     * @param state State of the board
+     * @return Function value
+     */
+    private static int h1(EightPuzzle state) {
+        int misplaced = 0;
+        // didn't want to think about how to write loop
+        if (state.grid[0][0] != 0) {
+            misplaced++;
+        }
+        if (state.grid[0][1] != 1) {
+            misplaced++;
+        }
+        if (state.grid[0][2] != 2) {
+            misplaced++;
+        }
+        if (state.grid[1][0] != 3) {
+            misplaced++;
+        }
+        if (state.grid[1][1] != 4) {
+            misplaced++;
+        }
+        if (state.grid[1][2] != 5) {
+            misplaced++;
+        }
+        if (state.grid[2][0] != 6) {
+            misplaced++;
+        }
+        if (state.grid[2][1] != 7) {
+            misplaced++;
+        }
+        if (state.grid[2][2] != 8) {
+            misplaced++;
+        }
+        return misplaced;
+    }
+
+    /**
+     * A* search using h2
+     * 
+     * @return Number of moves
+     * @throws OutOfMemoryError Max node limit exceeded
+     */
+    private int solveH2() throws OutOfMemoryError {
+        // min cost heap
+        PriorityQueue<EightPuzzle> pq = new PriorityQueue<>();
+
+        // keep track of visited states
+        Map<String, EightPuzzle> visited = new HashMap<>();
+
+        // count of generated nodes
+        int nodes = 0;
+
+        // add initial state to pq
+        pq.add(this);
+
+        while (!visited.containsKey("012345678") && !pq.isEmpty() && nodes <= maxNodes) {
+            EightPuzzle currState = pq.poll();
+            String gridString = gridToString(currState);
+            if (!visited.containsKey(gridString)) {
+                visited.put(gridString, currState);
+
+                List<String> validMoves = currState.getValidMoves();
+                for (String move : validMoves) {
+                    // generate and add child state if not already visited
+                    EightPuzzle child = currState.duplicate();
+                    if (move.equals("up")) {
+                        child.up();
+                    } else if (move.equals("down")) {
+                        child.down();
+                    } else if (move.equals("left")) {
+                        child.left();
+                    } else {
+                        child.right();
+                    }
+                    if (!visited.containsKey(gridToString(child))) {
+                        int depth = currState.value - h2(currState) + 1;
+                        child.parent = currState;
+                        child.value = depth + h2(child);
+                        pq.add(child);
+                        nodes++;
+                    }
+                }
+            }
+        }
+        
+        if (nodes > maxNodes) {
+            throw new OutOfMemoryError("Max node limit exceeded.");
+        }
+
+        // Extract path
+        EightPuzzle trav = null;
+        if (visited.containsKey("012345678")) {
+            trav = visited.get("012345678");
+            int moveCount = 0;
+            List<String> path = new LinkedList<>();
+            while (trav != null) {
+                path.add(trav.prevMove);
+                moveCount++;
+                trav = trav.parent;
+            }
+            path.remove(path.size()-1);
+            Collections.reverse(path);
+            System.out.println(path.toString());
+            return moveCount-1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Heuristic function based on Manhattan distance of tiles to correct spot
+     * 
+     * @param state State of the board
+     * @return Function value
+     */
+    private static int h2(EightPuzzle state) {
+        int[][] spots = {{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int[] spot = spots[state.grid[i][j]];
+                sum += Math.abs(spot[0] - i) + Math.abs(spot[1] - j);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Convert 2D array board to string
+     * 
+     * @param p puzzle to convert
+     * @return String of board state
+     */
+    private static String gridToString(EightPuzzle p) {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : p.grid) {
+            for (int x : row) {
+                sb.append(x);
+            }
+        }
+        return sb.toString();
     }
 
     @Override
@@ -200,13 +482,35 @@ public class EightPuzzle {
         return printOut.toString();
     }
 
+    @Override
+    public int compareTo(EightPuzzle o) {
+        return this.value - o.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof EightPuzzle) {
+            EightPuzzle p = (EightPuzzle) o;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (grid[i][j] != p.grid[i][j]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         EightPuzzle puzzle = new EightPuzzle();
-        String[] rows = {"234", "156", "078"};
-        System.out.println(puzzle.setState(rows));
-        System.out.println(puzzle.getValidMoves());
-        System.out.println(puzzle.toString());
-        puzzle.randomize(30);
+        String[] rows = {"158", "702", "346"};
+        puzzle.setState(rows);
+        puzzle.randomize(50);
         puzzle.toString();
+        EightPuzzle.setMaxNodes(500);
+        puzzle.solveAStar("h2");
+        puzzle.solveAStar("h1");
     }
 }
